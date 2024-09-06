cfd607818d05b2ab608711f697c156198343e4cc
Chopper <doc.chopperx@gmail.com>
2024-09-07 01:59:55 +0300

    updated MinHeap

--- include/min_heap.h
+++ include/min_heap.h
@@ -3,18 +3,25 @@
 
 #include <stdbool.h>
 
+typedef struct min_heap_node {
+    struct min_heap_node* left;
+    struct min_heap_node* right;
+    unsigned int freq;
+    char key;
+} min_heap_node_t;
+
+min_heap_node_t* new_min_heap_node(char, unsigned int);
+
 typedef struct {
     unsigned int size;
     unsigned int capacity;
-    unsigned int* array;
+    min_heap_node_t** array;
 } min_heap_t;
 
 bool new_min_heap(min_heap_t*);
 void free_min_heap(min_heap_t*);
-void min_heap_add(unsigned int, min_heap_t*);
-unsigned int min_heap_extract(min_heap_t*);
-
-void build_min_heap_O_nlogn(min_heap_t*, unsigned int*, unsigned int);
-bool build_min_heap_O_n(min_heap_t*, unsigned int*, unsigned int);
+void min_heap_add(min_heap_node_t*, min_heap_t*);
+min_heap_node_t* min_heap_extract(min_heap_t*);
+void build_min_heap(min_heap_t*);
 
 #endif // _MIN_HEAP_H_
--- src/min_heap.c
+++ src/min_heap.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DEFAULT_CAPACITY 11
+#define MIN_HEAP_CAPACITY 11
 
-static unsigned int default_min_heap_capacity = DEFAULT_CAPACITY;
+static unsigned int min_heap_capacity = MIN_HEAP_CAPACITY;
 
 static inline unsigned int left_child_idx(unsigned int);
 static inline unsigned int right_child_idx(unsigned int);
@@ -26,65 +26,83 @@ static inline unsigned int parent_idx(unsigned int i) {
 
 static void sift_up(int i, min_heap_t* minheap) {
 
-    unsigned int tmp = 0;
+    min_heap_node_t* swp = 0;
 
     while(i > 0 && minheap->array[parent_idx(i)] > minheap->array[i]) {
-        tmp = minheap->array[i]; 
+        swp = minheap->array[i]; 
         minheap->array[i] = minheap->array[parent_idx(i)];
-        minheap->array[parent_idx(i)] = tmp;
+        minheap->array[parent_idx(i)] = swp;
         i = parent_idx(i);
     }
 }
 
 static void sift_down(int i, min_heap_t* minheap) {
     int min_child;
+    min_heap_node_t* swp;
 
     if(left_child_idx(i) < minheap->size - 1 && minheap->array[left_child_idx(i)] < minheap->array[i])
         min_child = left_child_idx(i);
+    else
+        min_child = i;
 
     if(right_child_idx(i) < minheap->size - 1 && minheap->array[right_child_idx(i)] < minheap->array[min_child])
         min_child = right_child_idx(i);
 
     if(min_child != i) {
-        unsigned int tmp = minheap->array[min_child];
+        swp = minheap->array[min_child];
         minheap->array[min_child] = minheap->array[i];
-        minheap->array[i] = tmp;
+        minheap->array[i] = swp;
         sift_down(min_child, minheap);
     }
+}
+
+min_heap_node_t* new_min_heap_node(char key, unsigned int freq) {
+    min_heap_node_t* ret;
+
+    ret = (min_heap_node_t*) malloc(sizeof(min_heap_node_t));
+    ret->freq = freq;
+    ret->key = key;
+    ret->left = NULL;
+    ret->right = NULL;
+
+    return ret;
 }
 
 bool new_min_heap(min_heap_t * minheap) {
 
     unsigned int alloc;
-    alloc = default_min_heap_capacity * sizeof(unsigned int);
-    minheap->array = malloc(alloc);
+    alloc = min_heap_capacity * sizeof(unsigned int);
+    minheap->array = (min_heap_node_t**) malloc(alloc);
     if(minheap->array == NULL)
         return false;
 
     memset(minheap->array, 0, alloc);
 
     minheap->size = 0;
-    minheap->capacity = default_min_heap_capacity;
+    minheap->capacity = min_heap_capacity;
 
     return true;
 }
 
-void min_heap_add(unsigned int val, min_heap_t * minheap) {
-
-    minheap->array[minheap->size] = val;
-    minheap->size++;
-
-    if(minheap->size == minheap->capacity){
-        minheap->array = (unsigned int*) realloc(minheap->array, 2 * minheap->capacity);
-        minheap->capacity = 2 * minheap->capacity;
-    }
-    sift_up(minheap->size, minheap);
+void free_min_heap(min_heap_t * minheap) {
+    free(minheap->array);
 }
 
-unsigned int min_heap_extract(min_heap_t * minheap) {
-    unsigned int ret;
+void min_heap_add(min_heap_node_t* val, min_heap_t * minheap) {
+    int i = minheap->size;
+    ++minheap->size;
+
+    while(i > 0 && val->freq < minheap->array[parent_idx(i)]->freq) {
+        minheap->array[i] = minheap->array[parent_idx(i)];
+        i = parent_idx(i);
+    }
+    minheap->array[i] = val; 
+}
+
+min_heap_node_t* min_heap_extract(min_heap_t * minheap) {
+    min_heap_node_t* ret;
     if(minheap->size == 0)
-        return -1;
+        return NULL;
 
     ret = minheap->array[0];
 
@@ -95,10 +113,8 @@ unsigned int min_heap_extract(min_heap_t * minheap) {
     return ret;
 }
 
-void build_min_heap_O_nlogn(min_heap_t * minheap, unsigned int * arr, unsigned int arr_size) {
-
-    new_min_heap(minheap);
-
-    for(unsigned int i = 0; i < arr_size; ++i)
-        min_heap_add(arr[i], minheap);
+void build_min_heap(min_heap_t * minheap) {
+    for(unsigned int i = minheap->size / 2; i > 0; --i)
+        sift_down(i, minheap);
 }
+
